946087883543eddc298c5d5c67deebc9248e6a7d
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2024-09-06 08:27:35 +0600

    footer create complete

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AlertComponent } from './shared/alert/alert.component';
 import { NavbarComponent } from './shared/component/navbar/navbar.component';
+import { FooterComponent } from './shared/component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NgbModule, AlertComponent, NavbarComponent],
+  imports: [RouterOutlet, CommonModule, NgbModule, AlertComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
