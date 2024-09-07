90c4ef9fb96f8fda5c1bd36481311e3cb6fc7a65
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2024-09-07 07:56:20 +0600

    product service created

--- Frontend/src/app/features/product/product.service.ts
+++ Frontend/src/app/features/product/product.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BaseApiService } from '../../utility/base-api-service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../home/home.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
+export class ProductService extends BaseApiService{
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    super();
+  }
+
+  getProductById(id: string):Observable<Product>{
+    return this.http.get<Product>(`${this.baseurl}/product/id/${id}`);
+  }
+
+  getPicturesByProductId(id: string):Observable<any>{
+    return this.http.get<any>(`${this.baseurl}/product/${id}/pictures`);
+  }
+
+  getCommentsByProductId(id: string):Observable<any>{
+    return this.http.get<any>(`${this.baseurl}/product/${id}/comments`);
+  }
 }
