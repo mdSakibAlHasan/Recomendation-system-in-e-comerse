e5f13f6aad687650fc9cf71bcae219bac9560630
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2024-09-24 19:48:47 +0600

    update/delete brand frontent created

--- Frontend/src/app/features/super-user/super-user.component.ts
+++ Frontend/src/app/features/super-user/super-user.component.ts
@@ -20,6 +20,7 @@ export class SuperUserComponent implements OnInit{
   newCategory: string = '';
   categoryID: number = 0;
   newBrand: string = '';
+  brandID: number = 0;
   selectedCategory: string = '';
   selectedCategoryID: number = 0;
   constructor(
@@ -84,6 +85,7 @@ export class SuperUserComponent implements OnInit{
         this.alertService.tosterSuccess('Category updated');
         this.getCategory();
         this.categoryID = 0;
+        this.newCategory = '';
       }, 
       error: err=>{
         this.alertService.tosterDanger('Something went wrong');
@@ -98,6 +100,7 @@ export class SuperUserComponent implements OnInit{
       next: res=>{
         this.alertService.tosterSuccess('Category added');
         this.getCategory();
+        this.newCategory = '';
       }, 
       error: err=>{
         this.alertService.tosterDanger('Something went wrong');
@@ -111,15 +114,51 @@ export class SuperUserComponent implements OnInit{
       next: res=>{
         this.alertService.tosterSuccess('Brand added under '+this.selectedCategory);
         this.fetchBrands(this.selectedCategoryID);
+        this.newBrand = ''
       }, 
       error: err=>{
         this.alertService.tosterDanger('Something went wrong');
       }
     })
-    // this.brandService.addBrand({ name: this.newBrand, categoryId: this.selectedCategory }).subscribe(() => {
-    //   this.fetchBrands(this.selectedCategory);
-    //   this.newBrand = ''; // Clear input field
-    // });
+  }
+
+
+  brandEdit(brandID:number, newBrand: string){
+    this.brandID = brandID;
+    this.newBrand = newBrand;
+  }
+
+  brandDelete(brandID:number){
+    this.alertService.confirm(
+			'Are you want to delete this brand?',
+			() => {
+				this.superUnitService.deleteBrand(brandID).subscribe({
+					next: res => {
+						this.alertService.tosterSuccess("Brand delete complete");
+            this.fetchBrands(this.selectedCategoryID);
+					},
+					error: err => {
+						this.alertService.tosterDanger(err.message);
+					},
+				});
+			},
+			() => {}
+		);
+  }
+
+  onUpdateBrand(){
+    if (!this.newBrand) return;
+    this.superUnitService.updateBrand(this.brandID,{"name":this.newBrand}).subscribe({
+      next: res=>{
+        this.alertService.tosterSuccess('Brand updated');
+        this.fetchBrands(this.selectedCategoryID);
+        this.categoryID = 0;
+        this.newBrand = '';
+      }, 
+      error: err=>{
+        this.alertService.tosterDanger('Something went wrong');
+      }
+    })
   }
 
 }
