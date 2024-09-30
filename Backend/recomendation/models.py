eacaf23a42c2fd2eb056a39c70fa1c45a471c3c0
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2024-09-30 23:40:04 +0600

    recommendation model created

--- Backend/recomendation/models.py
+++ Backend/recomendation/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from user.models import User
+from product.models import Product
 
-# Create your models here.
+
+
+class VisitActivity(models.Model):
+    PID = models.ForeignKey('product.Product', on_delete=models.CASCADE , verbose_name='product ID')
+    UID = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='user ID')
+    times_vist = models.IntegerField(default=1)
+    last_time = models.DateTimeField(auto_now=True)
+
+
+class SearchActivity(models.Model):
+    UID = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='user ID', blank=True , null=True)
+    keyword = models.TextField(verbose_name=("Search keyword"))
