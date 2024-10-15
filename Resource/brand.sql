c19e3e7161f52ef3e47845defa3e15efcd62a2e0
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2024-10-15 19:23:15 +0600

    all product data added in database

--- Resource/brand.sql
+++ Resource/brand.sql
@@ -1,10 +1,18 @@
-INSERT INTO product_brand (name, categoryID_id) VALUES 
-('Apple', 1),  -- 1 is Mobile
-('Samsung', 1), 
-('Xiaomi', 1), 
-('OnePlus', 1), 
-('Lenovo', 2),  -- 2 is Laptop
-('Dell', 2), 
-('HP', 2), 
-('Asus', 2), 
-('Acer', 2);
+/*
+-- Query: SELECT * FROM shop.product_brand
+LIMIT 0, 1000
+
+-- Date: 2024-10-15 19:22
+*/
+INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (1,'Apple',1);
+INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (2,'Samsung',1);
+INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (3,'Xiaomi',1);
+INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (4,'OnePlus',1);
+INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (5,'Lenovo',2);
+INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (6,'Dell',2);
+INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (7,'HP',2);
+INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (8,'Asus',2);
+INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (9,'Acer',2);
+INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (10,'Casio',3);
+INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (12,'Unknown',2);
+INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (13,'Unknown',3);
