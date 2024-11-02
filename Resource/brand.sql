dd12b5f269c87163214b3fb2dba22c33f0c3205c
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2024-11-02 15:44:51 +0600

    category sql changes

--- Resource/brand.sql
+++ Resource/brand.sql
@@ -4,15 +4,15 @@ LIMIT 0, 1000
 
 -- Date: 2024-10-15 19:22
 */
-INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (1,'Apple',1);
-INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (2,'Samsung',1);
-INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (3,'Xiaomi',1);
-INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (4,'OnePlus',1);
-INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (5,'Lenovo',2);
-INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (6,'Dell',2);
-INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (7,'HP',2);
-INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (8,'Asus',2);
-INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (9,'Acer',2);
-INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (10,'Casio',3);
-INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (12,'Unknown',2);
-INSERT INTO `` (`id`,`name`,`categoryID_id`) VALUES (13,'Unknown',3);
+INSERT INTO `product_brand` (`id`,`name`,`categoryID_id`) VALUES (1,'Apple',1);
+INSERT INTO `product_brand` (`id`,`name`,`categoryID_id`) VALUES (2,'Samsung',1);
+INSERT INTO `product_brand` (`id`,`name`,`categoryID_id`) VALUES (3,'Xiaomi',1);
+INSERT INTO `product_brand` (`id`,`name`,`categoryID_id`) VALUES (4,'OnePlus',1);
+INSERT INTO `product_brand` (`id`,`name`,`categoryID_id`) VALUES (5,'Lenovo',2);
+INSERT INTO `product_brand` (`id`,`name`,`categoryID_id`) VALUES (6,'Dell',2);
+INSERT INTO `product_brand` (`id`,`name`,`categoryID_id`) VALUES (7,'HP',2);
+INSERT INTO `product_brand` (`id`,`name`,`categoryID_id`) VALUES (8,'Asus',2);
+INSERT INTO `product_brand` (`id`,`name`,`categoryID_id`) VALUES (9,'Acer',2);
+INSERT INTO `product_brand` (`id`,`name`,`categoryID_id`) VALUES (10,'Casio',3);
+INSERT INTO `product_brand` (`id`,`name`,`categoryID_id`) VALUES (12,'Unknown',2);
+INSERT INTO `product_brand` (`id`,`name`,`categoryID_id`) VALUES (13,'Unknown',3);
