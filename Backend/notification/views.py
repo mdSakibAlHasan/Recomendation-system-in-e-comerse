95c38efc790d83dc9c518489aa534c60747c9ad2
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2024-12-12 15:52:00 +0600

    notification status change code added

--- Backend/notification/views.py
+++ Backend/notification/views.py
@@ -19,7 +19,19 @@ class NotificationView(APIView):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+    
+    def patch(self, request, notification_id):
+        """
+        Marks the notification as read (changes is_read to True).
+        """
+        try:
+            notification = Notification.objects.get(id=notification_id, user=request.user)
+            notification.is_read = True
+            notification.save()
+            return Response({'status': 'Notification marked as read'}, status=200)
+        except Notification.DoesNotExist:
+            return Response({'error': 'Notification not found'}, status=404)
 
 
-def send_notification(user, message):
-    Notification.objects.create(user=user, message=message)
+def send_notification(user, message,link):      #create new notification from internal
+    Notification.objects.create(user=user, message=message, link=link)
