96482f07e9a46c707634e23137729ebff93f63c4
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2024-12-12 16:01:48 +0600

    Notification model change

--- Backend/notification/models.py
+++ Backend/notification/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from user.models import User
 
 class Notification(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='user ID')
--- Backend/notification/views.py
+++ Backend/notification/views.py
@@ -1,34 +1,44 @@
 # views.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from Backend.utils import getUserId
 from .models import Notification
 from .serializers import NotificationSerializer
 
 class NotificationView(APIView):
 
     def get(self, request):
-        notifications = Notification.objects.filter(user=request.user).order_by('-created_at')
-        serializer = NotificationSerializer(notifications, many=True)
-        return Response(serializer.data)
+        user_id = getUserId(request)
+        if user_id == None:
+            return Response({"message":"user not found"}, status=status.HTTP_403_FORBIDDEN)
+        else:
+            notifications = Notification.objects.filter(user_id=user_id).order_by('-created_at')
+            serializer = NotificationSerializer(notifications, many=True)
+            return Response(serializer.data)
 
-    def post(self, request):
-        data = request.data
-        data['user'] = request.user.id
-        serializer = NotificationSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+    # def post(self, request):
+    #     data = request.data
+    #     data['user'] = request.user.id
+    #     serializer = NotificationSerializer(data=data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=201)
+    #     return Response(serializer.errors, status=400)
     
     def patch(self, request, notification_id):
         """
         Marks the notification as read (changes is_read to True).
         """
         try:
-            notification = Notification.objects.get(id=notification_id, user=request.user)
-            notification.is_read = True
-            notification.save()
-            return Response({'status': 'Notification marked as read'}, status=200)
+            user_id = getUserId(request)
+            if user_id == None:
+                return Response({"message":"user not found"}, status=status.HTTP_403_FORBIDDEN)
+            else:
+                notification = Notification.objects.get(id=notification_id, user_id=user_id)
+                notification.is_read = True
+                notification.save()
+                return Response({'status': 'Notification marked as read'}, status=200)
         except Notification.DoesNotExist:
             return Response({'error': 'Notification not found'}, status=404)
 
