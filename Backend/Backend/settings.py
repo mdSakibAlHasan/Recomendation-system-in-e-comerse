b7ada8b90df4921cb1d089bca4757d44cb5381ef
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2024-12-19 19:14:46 +0600

    Real time notification websocket connection with handshake using instate memory

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -79,12 +79,9 @@ TEMPLATES = [
 ]
 
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],  # Replace with your Redis server details
-        },
-    },
+    'default':{
+        'BACKEND':'channels.layers.InMemoryChannelLayer'
+    }
 }
 
 
